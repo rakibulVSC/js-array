1e0d180dddf107fcb5e80ec79a0ab8a32a91a38d
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-10 22:08:53 +0600

    reverse array learning

--- loop.js
+++ loop.js
@@ -1,4 +1,18 @@
 const numbers=[12,57,42,85,83]
-for(const number of numbers){
-    console.log(number)
-}
+// for(const number of numbers){
+//     // console.log(number)
+// }
+
+
+// const length=numbers.length;
+// for(let i=0; i<numbers.length;i++){
+//     console.log('index value',i)
+    
+// }
+ let i=0;
+ while(i<numbers.length){
+    const item=numbers[i]
+    i++;
+    console.log(item)
+ }
+ 
---
+++ reverse.js
@@ -0,0 +1,3 @@
+const numbers=[12,57,42,85,83];
+const reversed=numbers.reverse();
+console.log(reversed)
