b9933beff0426dd57f0e10b63cba8d4454c597c8
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-10 22:19:30 +0600

    add reverse

--- reverse.js
+++ reverse.js
@@ -1,3 +1,16 @@
-const numbers=[12,57,42,85,83];
-const reversed=numbers.reverse();
-console.log(reversed)
+// // const numbers=[12,57,42,85,83];
+// // const reversed=numbers.reverse();
+// // console.log(reversed)
+
+// const friends=['abul','bavbubl','raihan','rion'];
+// const afterRevers=[];
+// for( const friend of friends){
+//     afterRevers.unshift(friend)
+//     console.log(afterRevers)
+// }
+
+
+const digits=[1,2,3,4,5,6]
+for (let i=digits.length-1;i>=0;i--){
+    console.log(i,digits[i])
+}
