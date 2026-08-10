6a20e85e27989ff3a936f4d52301aa3a5bbf4b49
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-10 22:42:16 +0600

    nested if condition

---
+++ prac.js
@@ -0,0 +1,18 @@
+for (let i=1;i<20;i++){
+    if( i%2 === 1){
+        // console.log(i)
+    }
+}
+
+for (let i=1;i<100;i++){
+    if(i%3 === 0 && i%5 === 0){
+        console.log("Fizz Buzz")
+    }
+    else if( i%3 === 0){
+        console.log("Fizz")
+    }
+    else if(i%5 === 0){
+        console.log("Buzz")
+    }
+    
+}
---
+++ slice.js
@@ -0,0 +1,6 @@
+const digits=[5,2,6,4,1,3];
+const slice=digits.slice(2,6);
+// console.log(slice);
+// console.log(digits)
+digits.sort()
+console.log(digits)
