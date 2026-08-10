b9fe013dbd4d2184e43561f2c2ade1896e36adf0
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-10 09:27:27 +0600

    pop of array

--- puspa.js
+++ puspa.js
@@ -1,3 +1,9 @@
 const friends=['robi','manik','sayem','alamin']
 friends.push('rajib')
-console.log(friends)
+// console.log(friends)
+
+
+const numbers=[1,2,3,4,5,6,7,8]
+// console.log(numbers)
+numbers.pop()
+console.log(numbers);
