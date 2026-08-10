73f4f38c55d6f4f602ee5e7c4ad25c3ab6ef007d
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-10 09:13:45 +0600

    learn length of array

--- aray.js
+++ aray.js
@@ -1,3 +1,7 @@
-// we learn this season array which can hold more than one values 
-const team=[1,12,23]
-console.log(team[1])
+// // we learn this season array which can hold more than one values 
+// const team=[1,12,23]
+// console.log(team[1])
+
+const fruits=['Mango','Lichi','Jackfruit','papaya','grape','grapefriut']
+const totalFruits=fruits.length;
+console.log(totalFruits)
