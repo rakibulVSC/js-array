99dba801b44b757dc4596cedabe1afbc61500c5e
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-10 22:53:45 +0600

    array value change

--- prac.js
+++ prac.js
@@ -1,18 +1,34 @@
-for (let i=1;i<20;i++){
-    if( i%2 === 1){
-        // console.log(i)
-    }
-}
+// for (let i=1;i<20;i++){
+//     if( i%2 === 1){
+//         // console.log(i)
+//     }
+// }
 
-for (let i=1;i<100;i++){
-    if(i%3 === 0 && i%5 === 0){
-        console.log("Fizz Buzz")
-    }
-    else if( i%3 === 0){
-        console.log("Fizz")
-    }
-    else if(i%5 === 0){
-        console.log("Buzz")
-    }
+// for (let i=1;i<100;i++){
+//     if(i%3 === 0 && i%5 === 0){
+//         console.log("Fizz Buzz")
+//     }
+//     else if( i%3 === 0){
+//         console.log("Fizz")
+//     }
+//     else if(i%5 === 0){
+//         console.log("Buzz")
+//     }
     
-}
+// }
+
+
+
+const array=["HTML", "CSS", "JavaScript", "React", "Node"];
+// console.log(array);
+const value=array[3];
+// console.log(value);
+// console.log(array.length);
+
+
+
+
+let skills = ["HTML", "CSS", "JavaScript", "React"];
+skills[1]="Tailwind";
+skills[3]="Node.js"
+console.log(skills)
