e21223d3892c8d3eabf453e01c016aced72a7c5b
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-10 09:34:31 +0600

    include element

--- shift.js
+++ shift.js
@@ -1,3 +1,9 @@
 const digits=[10,309,345,56,6,4,35]
 digits.unshift(11);
-console.log(digits)
+// console.log(digits)
+
+
+
+const numbers=[12,19,50,74,45]
+const hasItem=numbers.includes(12);
+console.log(hasItem);
